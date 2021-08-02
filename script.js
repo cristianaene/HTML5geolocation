4611d774314ce621b167e04211415b7ed8fd8eac
Cristiana Ene <cristiana.mihaela.ene@gmail.com>
2021-08-02 11:50:40 +0200

    adding draft

---
+++ script.js
@@ -0,0 +1,10 @@
+function printPosition() {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var positionInfo = "Your current position is: (" + "Latitude: " + position.coords.latitude + ", " + "Longitude: " + position.coords.longitude + ")";
+            document.getElementById("currentposition").innerHTML = positionInfo;
+        });
+    } else {
+        alert("We are sorry, your browser does not support HTML5 geolocation");
+    }
+}
